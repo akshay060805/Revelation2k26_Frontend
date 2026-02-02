1583ee82e3fe3a7467fb205946450e16e9d9c146
akshay060805 <vanama.akshay06@gmail.com>
2026-02-02 23:31:44 +0530

    Final commit

--- src/components/EventSlider/EventSlider2.jsx
+++ src/components/EventSlider/EventSlider2.jsx
@@ -1,31 +1,31 @@
 import { useEffect, useRef } from "react";
 import event1 from "../../assets/Event_img/BrainDead.webp";
 import event2 from "../../assets/Event_img/HackQuest.webp";
-import event3 from "../../assets/Event_img/Ludo.webp";
+// import event3 from "../../assets/Event_img/Ludo.webp";
 import event4 from "../../assets/Event_img/codeStorm.webp";
-import event5 from "../../assets/Event_img/Fifa.webp";
+// import event5 from "../../assets/Event_img/Fifa.webp";
 import event6 from "../../assets/Event_img/Ganitam.webp";
 import event7 from "../../assets/Event_img/MurderMystery.webp";
 import event8 from "../../assets/Event_img/RootAccess.webp";
 import event9 from "../../assets/Event_img/TechnoQuiz.webp";
-import event10 from "../../assets/Event_img/Valorant.webp";
+// import event10 from "../../assets/Event_img/Valorant.webp";
 import event11 from "../../assets/Event_img/BloodDonation.webp";
-import event12 from "../../assets/Event_img/Bgmi.webp";
+// import event12 from "../../assets/Event_img/Bgmi.webp";
 import "./EventSlider2.css";
 
 const events = [
   { id: 1, image: event1, title: "Brain Dead" },
   { id: 2, image: event2, title: "HackQuest" },
-  { id: 3, image: event3, title: "Ludo" },
+  { id: 3, image: event11, title: "BloodDonation" },
   { id: 4, image: event4, title: "Code Storm" },
-  { id: 5, image: event5, title: "FIFA" },
+  { id: 5, image: event1, title: "Brain Dead" },
   { id: 6, image: event6, title: "Ganitam" },
   { id: 7, image: event7, title: "Murder Mystery" },
   { id: 8, image: event8, title: "Root Access" },
   { id: 9, image: event9, title: "Techno Quiz" },
-  { id: 10, image: event10, title: "Valorant" },
+  { id: 10, image: event4, title: "Code Storm" },
   { id: 11, image: event11, title: "BloodDonation" },
-  { id: 12, image: event12, title: "Bgmi" }
+  { id: 12, image: event6, title: "Ganitam" }
 ];
 
 function EventSlider() {
